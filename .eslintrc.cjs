4353757778fb30bfea6e9048a9505f4c29dbd8fe
Lucas <lucas.ribeiro711@gmail.com>
2022-09-04 01:22:13 -0300

    chore: :package: config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,16 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  }
-}
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
